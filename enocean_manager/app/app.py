09dc8b9804cbdc43375f00db8710e5851d4743c3
maximedub <maximedub@users.noreply.github.com>
2025-08-09 09:06:29 +0200

    Update app.py

--- enocean_manager/app/app.py
+++ enocean_manager/app/app.py
@@ -1,9 +1,9 @@
 # app/app.py
 from flask import Flask, jsonify, render_template
-from .communicator import communicator
-from .pairing import start_pairing_listener  # si tu as une fonction; sinon adapte
-from .devices import get_devices
-from .eep_parser import download_eep_file, parse_eep_file
+from enocean_manager.communicator import communicator
+from enocean_manager.pairing import start_pairing_listener  # si tu as une fonction; sinon adapte
+from enocean_manager.devices import get_devices
+from enocean_manager.eep_parser import download_eep_file, parse_eep_file
 
 app = Flask(__name__, template_folder="templates")
 
